3f34a939b501c9430a06bea0b589569d7e899351
Jesus Paez <jpaez13@cuc.edu.co>
2022-04-23 19:22:53 -0500

    crud básico con firebase terminado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -11,8 +11,9 @@ const Formulario = () => {
     const [color, setColor] = React.useState('')
     const [precio, setPrecio] = React.useState('')
     const [lista, setLista] = React.useState([])
-    //const [id, setId] = React.useState('')
-    
+    const [modoEdicion, setModoEdicion] = React.useState(false)
+    const [id, setId] = React.useState('')
+    const [error, setError] = React.useState(null)
     
     React.useEffect(()=>{
         const obtenerDatos = async () =>{
@@ -36,6 +37,42 @@ const Formulario = () => {
 
     const guardarDatos = async(e) => {
         e.preventDefault()
+
+        if(!tipo.trim()){
+            setError('Campo tipo vacío')
+            return
+        }
+
+        if(!estado.trim()){
+            setError('Campo estado vacío')
+            return
+        }
+
+        if(!marca.trim()){
+            setError('Campo marca vacío')
+            return
+        }
+
+        if(!modelo.trim()){
+            setError('Campo modelo vacío')
+            return
+        }
+
+        if(!cilindraje.trim()){
+            setError('Campo color vacío')
+            return
+        }
+
+        if(!color.trim()){
+            setError('Campo color vacío')
+            return
+        }
+
+        if(!precio.trim()){
+            setError('Campo precio vacío')
+            return
+        }
+
         try{
             const db = firebase.firestore()
             const nuevoVehiculo = {
@@ -55,6 +92,16 @@ const Formulario = () => {
         }catch(error){
             console.log(error)
         }
+
+        setModoEdicion(false)
+        setTipo('')
+        setEstado('')
+        setMarca('')
+        setModelo('')
+        setCilindraje('')
+        setColor('')
+        setPrecio('')
+        setError(null)
        
     }
 
@@ -67,6 +114,93 @@ const Formulario = () => {
         }catch(error){
             console.log(error)
         }
+    }
+
+    const auxEditar = (item) =>{
+        setTipo(item.nombreTipo)
+        setPrecio(item.nombrePrecio)
+        setModelo(item.nombreModelo)
+        setMarca(item.nombreMarca)
+        setEstado(item.nombreEstado)
+        setColor(item.nombreColor)
+        setCilindraje(item.nombreCilindraje)
+        setModoEdicion(true)
+        setId(item.id)
+    }
+
+    const editar = async e =>{
+        e.preventDefault()
+        if(!tipo.trim()){
+            setError('Campo tipo vacío')
+            return
+        }
+
+        if(!estado.trim()){
+            setError('Campo estado vacío')
+            return
+        }
+
+        if(!marca.trim()){
+            setError('Campo marca vacío')
+            return
+        }
+
+        if(!modelo.trim()){
+            setError('Campo modelo vacío')
+            return
+        }
+
+        if(!cilindraje.trim()){
+            setError('Campo color vacío')
+            return
+        }
+
+        if(!color.trim()){
+            setError('Campo color vacío')
+            return
+        }
+
+        if(!precio.trim()){
+            setError('Campo precio vacío')
+            return
+        }
+        try{
+            const db = firebase.firestore()
+            await db.collection('vehiculos').doc(id).update({
+                nombreTipo:tipo,
+                nombrePrecio:precio,
+                nombreModelo:modelo,
+                nombreMarca:marca,
+                nombreEstado:estado,
+                nombreColor:color,
+                nombreCilindraje:cilindraje
+            })
+            
+        }catch(error){
+            console.log(error)
+        }
+        setTipo('')
+        setEstado('')
+        setMarca('')
+        setModelo('')
+        setCilindraje('')
+        setColor('')
+        setPrecio('')
+        setModoEdicion(false)
+        setError(null)
+        
+    }
+
+    const cancelar =() =>{
+        setTipo('')
+        setEstado('')
+        setMarca('')
+        setModelo('')
+        setCilindraje('')
+        setColor('')
+        setPrecio('')
+        setModoEdicion(false)
+        setError(null)
     }
 
     
@@ -85,57 +219,80 @@ const Formulario = () => {
                             - {item.nombreModelo} - {item.nombreCilindraje} - {item.nombreColor}
                             - {item.nombrePrecio}</span>
                             <button className='btn-danger btn-sm float-end mx-2'onClick={()=> eliminar(item.id)}>Eliminar</button>
-                            <button className='btn-warning btn-sm float-end mx-2'>Editar</button>
+                            <button className='btn-warning btn-sm float-end mx-2'onClick={()=> auxEditar(item)}>Editar</button>
                         </li> 
                         ))
                     }
                 </div>
                 <div className="col-4">
-                    <h4 className="text-center">Agregar vehículo</h4>
-                    <form onSubmit={guardarDatos}>
+                    <h4 className="text-center">
+                    {
+                        modoEdicion ? 'Editar vehículo': 'Agregar vehículo'
+                    }</h4>
+                    <form onSubmit={modoEdicion ? editar: guardarDatos}>
+                        {
+                            error ? <span className='text-danger'>{error}</span> : null
+                        }
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese tipo'
                             onChange={(e)=>setTipo(e.target.value)}
+                            value = {tipo}
                         />
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese estado'
                             onChange={(e)=>setEstado(e.target.value)}
+                            value = {estado}
                         />
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese marca'
                             onChange={(e)=>setMarca(e.target.value)}
+                            value = {marca}
                         />
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese modelo'
                             onChange={(e)=>setModelo(e.target.value)}
+                            value = {modelo}
                         />
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese cilindraje'
                             onChange={(e)=>setCilindraje(e.target.value)}
+                            value = {cilindraje}
                         />
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese color'
                             onChange={(e)=>setColor(e.target.value)}
+                            value = {color}
                         />
                         <input
                             className='form-control mb-2'
                             type="text"
                             placeholder='Ingrese precio'
                             onChange={(e)=>setPrecio(e.target.value)}
-                        />
-                        <button className='btn btn-primary btn-block'type='submit'>Agregar</button>
+                            value = {precio}
+                        />{
+                            !modoEdicion ? (
+                                <button className='btn btn-primary btn-block'type='submit'>Agregar</button>
+                            )
+                            :
+                            ( <>
+                            <button className='btn btn-warning btn-block'type='submit'>Editar</button>
+                            <button className='btn btn-dark btn-block mx-2'onClick={() => cancelar()}>Cancelar</button>
+                            </>
+                            )
+                        }
+                        
                      </form>
                 </div>
             </div>  
